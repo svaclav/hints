09d1299956d4abc5ca6ede100f08916480b0fb64
Vaclav Strachon <vaclav.strachon@gmail.com>
2016-11-10 14:00:05 +0100

    ping stats with awk

--- ping.sh
+++ ping.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-while [ 1 ]; do echo -n "$(date +%Y-%m-%d-%H:%M:%S)   " >> /tmp/ping.txt; ping -c 1 8.8.8.8 | sed -n '2p' | cut -d "=" -f 4 | cut -d " "  -f 1 >> /tmp/ping.txt; sleep 1; done
+ping -D -c 1 8.8.8.8 | awk '/icmp_seq/ { print $1 " -- " substr($8, 6, 10)}'
+
+#while [ 1 ]; do echo -n "$(date +%Y-%m-%d-%H:%M:%S)   " >> /tmp/ping.txt; ping -c 1 8.8.8.8 | sed -n '2p' | cut -d "=" -f 4 | cut -d " "  -f 1 >> /tmp/ping.txt; sleep 1; done
